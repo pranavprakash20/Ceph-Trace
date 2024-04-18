06719654ec974736d5601f651c3b740e443858c4
Pranav Prakash <prprakas@redhat.com>
2024-04-18 19:16:25 +0530

    [Lib] Add daemon class
    Signed-off-by: Pranav Prakash <prprakas@redhat.com>

---
+++ daemon/daemon.py
@@ -0,0 +1,18 @@
+# Class that handles the creation of perf-mon daemon
+
+class Daemon:
+
+    def __init(self):
+        pass
+
+    def create(self, name, service_content):
+        pass
+
+    def start(self):
+        pass
+
+    def status(self):
+        pass
+
+    def stop(self):
+        pass
